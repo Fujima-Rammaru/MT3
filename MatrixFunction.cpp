bbc96100be2bfd2452f1dbf46145be458242546d
藤間嵐丸 <146901376+Fujima-Rammaru@users.noreply.github.com>
2024-04-23 15:05:48 +0900

    マトリックス関数のクラス化

---
+++ MatrixFunction.cpp
@@ -0,0 +1,19 @@
+#include "MatrixFunction.h"
+
+Matrix4x4 MatrixFunction::MakeTranslateMatrix(const Vector3& translate)
+{
+
+
+
+    return Matrix4x4();
+}
+
+Matrix4x4 MatrixFunction::MakeScaleMatrix(const Vector3& scale)
+{
+    return Matrix4x4();
+}
+
+Vector3 MatrixFunction::Transform(const Vector3& vector, const Matrix4x4& matrix)
+{
+    return Vector3();
+}
---
+++ MatrixFunction.h
@@ -0,0 +1,24 @@
+#pragma once
+#include"Matrix4x4.h"
+#include"Vector3.h"
+
+class MatrixFunction {
+
+public:
+	
+	Matrix4x4 MakeTranslateMatrix(const Vector3& translate);
+
+	Matrix4x4 MakeScaleMatrix(const Vector3& scale);
+
+	Vector3 Transform(const Vector3& vector, const Matrix4x4& matrix);
+
+
+private:
+
+
+
+
+
+
+
+};
