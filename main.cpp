b1bb8329d3962c6e9b8afd30add75f615e2cb28a
藤間嵐丸 <146901376+Fujima-Rammaru@users.noreply.github.com>
2024-04-23 16:52:27 +0900

    行列関数の実装及び表示

--- main.cpp
+++ main.cpp
@@ -15,74 +15,86 @@ static void MatrixScreenPrintf(int x, int y, const Matrix4x4& matrix, const char
 			Novice::ScreenPrintf(x + column * kColumnWidth, y + row * kRowHeight + 17, "%6.02f", matrix.m[row][column]);
 		}
 	}
+}
+
+//３次元ベクトルの数値表現
+void VectorScreenPrintf(int x, int y, const Vector3& vector, const char* label) {
+	Novice::ScreenPrintf(x, y, "%.02f", vector.x);
+	Novice::ScreenPrintf(x + kColumnWidth, y, "%.02f", vector.y);
+	Novice::ScreenPrintf(x + kColumnWidth * 2, y, "%.02f", vector.z);
+	Novice::ScreenPrintf(x + kColumnWidth * 3, y, "%s", label);
+}
 
 
-	// Windowsアプリでのエントリーポイント(main関数)
-	int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
-		MatrixFunction* matrixFunction;
-		matrixFunction = new MatrixFunction;
+// Windowsアプリでのエントリーポイント(main関数)
+int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
-		// ライブラリの初期化
-		Novice::Initialize(kWindowTitle, 1280, 720);
+	MatrixFunction* matrixFunction;
+	matrixFunction = new MatrixFunction;
 
-		Vector3 translate = { 4.1f,2.6f,0.8f };
-		Vector3 scale = { 1.5f,5.2f,7.3f };
+	// ライブラリの初期化
+	Novice::Initialize(kWindowTitle, 1280, 720);
+
+	Vector3 translate = { 4.1f,2.6f,0.8f };
+	Vector3 scale = { 1.5f,5.2f,7.3f };
 
 
-		Matrix4x4 translatematrix = matrixFunction->MakeTranslateMatrix(translate);
-		Matrix4x4 scaleMatrix = matrixFunction->MakeScaleMatrix(scale);
+	Matrix4x4 translatematrix = matrixFunction->MakeTranslateMatrix(translate);
+	Matrix4x4 scaleMatrix = matrixFunction->MakeScaleMatrix(scale);
 
-		Vector3 point = { 2.3f,3.8f,1.4f };
+	Vector3 point = { 2.3f,3.8f,1.4f };
 
-		Matrix4x4 transformMatrix = {
-			1.0f,2.0f,3.0f,4.0f,
-			3.0f,1.0f,1.0f,2.0f,
-			1.0f,4.0f,2.0f,3.0f,
-			2.0f,2.0f,1.0f,3.0f,
-		};
+	Matrix4x4 transformMatrix = {
+		1.0f,2.0f,3.0f,4.0f,
+		3.0f,1.0f,1.0f,2.0f,
+		1.0f,4.0f,2.0f,3.0f,
+		2.0f,2.0f,1.0f,3.0f,
+	};
 
-		Vector3 transformed = matrixFunction->Transform(point, transformMatrix);
-		// キー入力結果を受け取る箱
-		char keys[256] = { 0 };
-		char preKeys[256] = { 0 };
+	Vector3 transformed = matrixFunction->Transform(point, transformMatrix);
+	// キー入力結果を受け取る箱
+	char keys[256] = { 0 };
+	char preKeys[256] = { 0 };
 
-		// ウィンドウの×ボタンが押されるまでループ
-		while (Novice::ProcessMessage() == 0) {
-			// フレームの開始
-			Novice::BeginFrame();
+	// ウィンドウの×ボタンが押されるまでループ
+	while (Novice::ProcessMessage() == 0) {
+		// フレームの開始
+		Novice::BeginFrame();
 
-			// キー入力を受け取る
-			memcpy(preKeys, keys, 256);
-			Novice::GetHitKeyStateAll(keys);
+		// キー入力を受け取る
+		memcpy(preKeys, keys, 256);
+		Novice::GetHitKeyStateAll(keys);
 
-			///
-			/// ↓更新処理ここから
-			///
+		///
+		/// ↓更新処理ここから
+		///
 
-			///
-			/// ↑更新処理ここまで
-			///
+		///
+		/// ↑更新処理ここまで
+		///
 
-			///
-			/// ↓描画処理ここから
-			///
+		///
+		/// ↓描画処理ここから
+		///
+		VectorScreenPrintf(0, 0, transformed, "transformed");
+		MatrixScreenPrintf(0, 20, translatematrix, "translateMatrix");
+		MatrixScreenPrintf(0, kRowHeight * 5 + 20, scaleMatrix, "scaleMatrix");
+		///
+		/// ↑描画処理ここまで
+		///
 
-			///
-			/// ↑描画処理ここまで
-			///
+		// フレームの終了
+		Novice::EndFrame();
 
-			// フレームの終了
-			Novice::EndFrame();
-
-			// ESCキーが押されたらループを抜ける
-			if (preKeys[DIK_ESCAPE] == 0 && keys[DIK_ESCAPE] != 0) {
-				break;
-			}
+		// ESCキーが押されたらループを抜ける
+		if (preKeys[DIK_ESCAPE] == 0 && keys[DIK_ESCAPE] != 0) {
+			break;
 		}
-
-		// ライブラリの終了
-		delete matrixFunction;
-		Novice::Finalize();
-		return 0;
 	}
+
+	// ライブラリの終了
+	delete matrixFunction;
+	Novice::Finalize();
+	return 0;
+}
