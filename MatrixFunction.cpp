45d993ee53b21b9d61c329372a96dba5b32c1827
藤間嵐丸 <146901376+Fujima-Rammaru@users.noreply.github.com>
2024-05-14 10:23:33 +0900

    01_00確認課題完了

--- MatrixFunction.cpp
+++ MatrixFunction.cpp
@@ -234,20 +234,20 @@ Matrix4x4 MatrixFunction::MakeAffineMatrix(const Vector3& scale, const Vector3& 
 	resultRZ.m[3][3] = 1;
 
 	//=========================
-	rotateXYZMatrix = Multiply(resultRX, Multiply(resultRY, resultRZ));
+	rotateXYZMatrix = Multiply(resultRX, Multiply(resultRY, resultRZ));//全軸の回転行列
 
 
 	//=========================
-	result1.m[0][0] =resultScale.m[0][0]*rotateXYZMatrix.m[0][0];
+	result1.m[0][0] = resultScale.m[0][0] * rotateXYZMatrix.m[0][0];
 	result1.m[0][1] = resultScale.m[0][0] * rotateXYZMatrix.m[0][1];
 	result1.m[0][2] = resultScale.m[0][0] * rotateXYZMatrix.m[0][2];
 	result1.m[0][3] = 0;
-	
+
 	result1.m[1][0] = resultScale.m[1][1] * rotateXYZMatrix.m[1][0];
 	result1.m[1][1] = resultScale.m[1][1] * rotateXYZMatrix.m[1][1];
 	result1.m[1][2] = resultScale.m[1][1] * rotateXYZMatrix.m[1][2];
 	result1.m[1][3] = 0;
-	
+
 	result1.m[2][0] = resultScale.m[2][2] * rotateXYZMatrix.m[2][0];
 	result1.m[2][1] = resultScale.m[2][2] * rotateXYZMatrix.m[2][1];
 	result1.m[2][2] = resultScale.m[2][2] * rotateXYZMatrix.m[2][2];
@@ -283,4 +283,89 @@ Matrix4x4 MatrixFunction::Multiply(const Matrix4x4& m1, const Matrix4x4& m2)
 	result.m[2][3] = m1.m[2][0] * m2.m[0][3] + m1.m[2][1] * m2.m[1][3] + m1.m[2][2] * m2.m[2][3] + m1.m[2][3] * m2.m[3][3];
 	result.m[3][3] = m1.m[3][0] * m2.m[0][3] + m1.m[3][1] * m2.m[1][3] + m1.m[3][2] * m2.m[2][3] + m1.m[3][3] * m2.m[3][3];
 	return result;
+}
+
+//正射影行列
+Matrix4x4 MatrixFunction::MakeOrthographicMatrix(float left, float top, float right, float bottom, float nearClip, float farClip)
+{
+	Matrix4x4 result;
+	result.m[0][0] = 2 / (right - left);
+	result.m[0][1] = 0;
+	result.m[0][2] = 0;
+	result.m[0][3] = 0;
+
+	result.m[1][0] = 0;
+	result.m[1][1] = 2 / (top - bottom);
+	result.m[1][2] = 0;
+	result.m[1][3] = 0;
+
+	result.m[2][0] = 0;
+	result.m[2][1] = 0;
+	result.m[2][2] = 1 / (farClip - nearClip);
+	result.m[2][3] = 0;
+
+	result.m[3][0] = (left + right) / (left - right);
+	result.m[3][1] = (top + bottom) / (bottom - top);
+	result.m[3][2] = nearClip / (nearClip - farClip);
+	result.m[3][3] = 1;
+
+	return result;
+}
+
+//透視投影行列
+Matrix4x4 MatrixFunction::MakePerspectiveFovMatrix(float fovY, float aspectRatio, float nearClip, float farClip)
+{
+	Matrix4x4 result;
+	float cot = 1 / std::tan(fovY / 2);
+	result.m[0][0] = 1 / aspectRatio * cot;
+	result.m[0][1] = 0;
+	result.m[0][2] = 0;
+	result.m[0][3] = 0;
+
+	result.m[1][0] = 0;
+	result.m[1][1] = cot;
+	result.m[1][2] = 0;
+	result.m[1][3] = 0;
+
+	result.m[2][0] = 0;
+	result.m[2][1] = 0;
+	result.m[2][2] = farClip / (farClip - nearClip);
+	result.m[2][3] = 1;
+
+	result.m[3][0] = 0;
+	result.m[3][1] = 0;
+	result.m[3][2] = (-nearClip * farClip) / (farClip - nearClip);
+	result.m[3][3] = 0;
+
+	return result;
+}
+
+//ビューポート変換行列
+Matrix4x4 MatrixFunction::MakeViewPortMatrix(float left, float top, float width, float height, float minDepth, float maxDepth)
+{
+	Matrix4x4 result;
+	result.m[0][0] = width / 2;
+	result.m[0][1] = 0;
+	result.m[0][2] = 0;
+	result.m[0][3] = 0;
+
+	result.m[1][0] = 0;
+	result.m[1][1] = -height / 2;
+	result.m[1][2] = 0;
+	result.m[1][3] = 0;
+
+	result.m[2][0] = 0;
+	result.m[2][1] = 0;
+	result.m[2][2] = maxDepth - minDepth;
+	result.m[2][3] = 0;
+
+	result.m[3][0] = left + (width / 2);
+	result.m[3][1] = top + (height / 2);
+	result.m[3][2] = minDepth;
+	result.m[3][3] = 1;
+
+
+
+	return result;
+
 }
--- main.cpp
+++ main.cpp
@@ -36,11 +36,9 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	// ライブラリの初期化
 	Novice::Initialize(kWindowTitle, 1280, 720);
 
-	Vector3 scale{ 1.2f,0.79f,-2.1f };//拡縮
-	Vector3 rotate{ 0.4f,1.43f,-0.8f };//回転
-	Vector3 translate{ 2.7f,-4.15f,1.57f };//平行移動
-
-	Matrix4x4 worldMatrix = matrixFunction->MakeAffineMatrix(scale, rotate, translate);
+	Matrix4x4 orthographicMatrix = matrixFunction->MakeOrthographicMatrix(-160.0f,160.0f,200.0f,300.0f,0.0f,1000.0f);
+	Matrix4x4 perspectiveFovMatrix = matrixFunction->MakePerspectiveFovMatrix(0.63f,1.33f,0.1f,1000.0f);
+	Matrix4x4 viewportMatrix = matrixFunction->MakeViewPortMatrix(100.0f,200.0f,600.0f,300.0f,0.0f,1.0f);
 	// キー入力結果を受け取る箱
 	char keys[256] = { 0 };
 	char preKeys[256] = { 0 };
@@ -65,7 +63,9 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		///
 		/// ↓描画処理ここから
 		///
-		MatrixScreenPrintf(0, 0, worldMatrix, "worldMatrix");
+		MatrixScreenPrintf(0, 0, orthographicMatrix, "orthographicMatrix");
+		MatrixScreenPrintf(0, kRowHeight*5, perspectiveFovMatrix, "perspectiveFovMatrix");
+		MatrixScreenPrintf(0,kRowHeight*10, viewportMatrix, "viewportMatrix");
 		///
 		/// ↑描画処理ここまで
 		///
