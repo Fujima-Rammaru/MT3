265652f5f748290d64acafd7bd113a1ea433ff97
藤間嵐丸 <146901376+Fujima-Rammaru@users.noreply.github.com>
2024-07-19 16:19:44 +0900

    a

---
+++ MT3.h
@@ -0,0 +1,11 @@
+#pragma once
+#include"Vector3.h"
+#include"matrix4x4.h"
+float Dot(const Vector3& v1,const Vector3& v2) {
+	return v1.x * v2.x + v1.y * v2.y + v1.z * v2.z;
+}
+Vector3 Project(const Vector3& v1, const Vector3& v2) {
+	float v25qLength =
+
+
+}
